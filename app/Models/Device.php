88a0f5e0bd3a920cd9fb940798eaf66a5b34f427
Bogdan <gewaldb@gmail.com>
2025-11-01 21:44:02 +0100

    Comment out unused guest routes in `auth.php` and update PHP version in project guidelines.

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -5,21 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-/**
- * @property int $id
- * @property int $user_id
- * @property string $name
- * @property string $secret_key
- * @property string|null $wifi_ssid
- * @property string|null $wifi_password
- * @property \Illuminate\Support\Carbon|null $last_seen_at
- * @property bool $is_active
- * @property string|null $firmware_version
- * @property string|null $ip_last
- * @property string|null $mac_address
- * @property array|null $metadata
- * @property array|null $settings
- */
 final class Device extends Model
 {
     protected $guarded = ['id'];
