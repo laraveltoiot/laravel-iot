75cf21cfb144fe52c23335aefb5750d7dc4665df
Bogdan <gewaldb@gmail.com>
2025-11-02 13:27:40 +0100

    Refactor `UserFactory` to use `$this->faker` and update return types for improved consistency.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,8 +23,8 @@ final class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => self::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
@@ -37,7 +37,7 @@ final class UserFactory extends Factory
     /**
      * Indicate that the model's email address should be unverified.
      */
-    public function unverified(): UserFactory
+    public function unverified(): self
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
@@ -47,7 +47,7 @@ final class UserFactory extends Factory
     /**
      * Indicate that the model does not have two-factor authentication configured.
      */
-    public function withoutTwoFactor(): UserFactory
+    public function withoutTwoFactor(): self
     {
         return $this->state(fn (array $attributes) => [
             'two_factor_secret' => null,
